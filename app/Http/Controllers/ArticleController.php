61b9d54f079f4fdab7f54680fe20a74baaaad6b7
Yoshinori Shimoda <ys@platinum.local>
2021-05-30 21:50:07 +0900

    first commit

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+//==========ここから追加==========
+use App\Article;
+//==========ここまで追加==========
+use Illuminate\Http\Request;
+
+class ArticleController extends Controller
+{    
+    public function index()
+    {
+        //==========ここから追加==========
+        $articles = Article::all()->sortByDesc('created_at');
+        //==========ここまで追加==========
+
+        return view('articles.index', ['articles' => $articles]);
+    }
+}
