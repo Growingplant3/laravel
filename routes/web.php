0ca7a63a3a3f637958e8d881242bdcb7e2c6d50b
Yoshinori Shimoda <ys@platinum.local>
2021-06-09 08:14:36 +0900

    ルーティングの追記/改修

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,5 @@
 <?php
 
 Auth::routes(); //-- この行を追加
-Route::get('/', 'ArticleController@index');
+Route::get('/', 'ArticleController@index')->name('articles.index'); //-- この行を編集
+Route::resource('/articles', 'ArticleController')->except(['index']); //-- この行を編集
