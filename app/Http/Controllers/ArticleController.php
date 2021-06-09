c6146370406213f56bf4e86d4cd284eb338bc5cb
Yoshinori Shimoda <ys@platinum.local>
2021-06-10 08:17:21 +0900

    createアクションを追加

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,19 +2,20 @@
 
 namespace App\Http\Controllers;
 
-//==========ここから追加==========
 use App\Article;
-//==========ここまで追加==========
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {    
     public function index()
     {
-        //==========ここから追加==========
         $articles = Article::all()->sortByDesc('created_at');
-        //==========ここまで追加==========
 
         return view('articles.index', ['articles' => $articles]);
+    }
+
+    public function create()
+    {
+        return view('articles.create');    
     }
 }
