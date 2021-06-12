84caa056487561c2986253c5cbe1d0b42a56516c
Yoshinori Shimoda <ys@platinum.local>
2021-06-12 13:30:01 +0900

    リクエストクラスのインスタンスを引数としてcreateアクションを記述/editアクションを追加

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Article;
+//===========ここから追加==========
+use App\Http\Requests\ArticleRequest;
+//===========ここまで追加==========
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
@@ -17,5 +20,18 @@ class ArticleController extends Controller
     public function create()
     {
         return view('articles.create');    
+    }
+
+    public function store(ArticleRequest $request, Article $article)
+    {
+        $article->fill($request->all());
+        $article->user_id = $request->user()->id;
+        $article->save();
+        return redirect()->route('articles.index');
+    }
+
+    public function edit(Article $article)
+    {
+        return view('articles.edit', ['article' => $article]);    
     }
 }
