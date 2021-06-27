1bf8d389c293237e3c32ecb67940d92819e56974
Yoshinori Shimoda <ys@platinum.local>
2021-06-27 10:22:33 +0900

    ルーティングを拡張

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,10 @@
 
 Auth::routes();
 Route::get('/', 'ArticleController@index')->name('articles.index');
-Route::resource('/articles', 'ArticleController')->except(['index', 'show'])->middleware('auth'); //-- exceptメソッドの引数を変更
-Route::resource('/articles', 'ArticleController')->only(['show']); //-- この行を追加
+Route::resource('/articles', 'ArticleController')->except(['index', 'show'])->middleware('auth');
+Route::resource('/articles', 'ArticleController')->only(['show']);
 Route::prefix('articles')->name('articles.')->group(function () {
   Route::put('/{article}/like', 'ArticleController@like')->name('like')->middleware('auth');
   Route::delete('/{article}/like', 'ArticleController@unlike')->name('unlike')->middleware('auth');
 });
+Route::get('/tags/{name}', 'TagController@show')->name('tags.show');
